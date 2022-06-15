15809e42ab9e90bb29258e1780a9c54a1fca8c90
vacto.wang <vacto.wang@vungle.com>
2022-06-15 16:16:54 +0800

    modify demo tests

--- examples/consumer/tests/consumer/test_ads_consumer.py
+++ examples/consumer/tests/consumer/test_ads_consumer.py
@@ -1,16 +1,15 @@
-"""pact test for the Bastion service client"""
+"""pact test for the Jaeger service client"""
 
 import atexit
 import logging
 import os
-
 import pytest
 
 from examples.common.test_data.data import *
-from examples.consumer.src.consumer import AdsConsumer
+from examples.common.test_data.expected_response_payload import *
 from examples.common.test_data.headers import *
 from examples.common.test_data.request_payload import *
-from examples.common.test_data.expected_response_payload import *
+from examples.consumer.src.consumer import AdsConsumer
 from pact import Consumer, Like, Provider
 
 log = logging.getLogger(__name__)
@@ -25,32 +24,19 @@ PACT_BROKER_URL = "http://localhost"
 PACT_BROKER_USERNAME = "pactbroker"
 PACT_BROKER_PASSWORD = "pactbroker"
 
-# pactflow
-# PACT_BROKER_URL = "https://ssptest.pactflow.io/"
-# PACT_BROKER_TOKEN = "_cmJAJb1Zos1DkcTUivTMw"
-
-# Define where to run the mock server, for the consumer to connect to. These
-# are the defaults so may be omitted
 PACT_MOCK_HOST = "localhost"
 PACT_MOCK_PORT = 1234
 
-# Where to output the JSON Pact files created by any tests
 PACT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 @pytest.fixture
-def consumer() -> AdsConsumer:
+def ads_consumer() -> AdsConsumer:
     return AdsConsumer("http://{host}:{port}".format(host=PACT_MOCK_HOST, port=PACT_MOCK_PORT))
 
 
 @pytest.fixture(scope="session")
 def pact(request):
-    """Setup a Pact Consumer, which provides the Provider mock service. This
-    will generate and optionally publish Pacts to the Pact Broker"""
-
-    # When publishing a Pact to the Pact Broker, a version number of the Consumer
-    # is required, to be able to construct the compatability matrix between the
-    # Consumer versions and Provider versions
     version = request.config.getoption("--publish-pact")
     publish = True if version else False
 
@@ -62,108 +48,213 @@ def pact(request):
         publish_to_broker=publish,
         broker_base_url=PACT_BROKER_URL,
         broker_username=PACT_BROKER_USERNAME,
-        broker_password=PACT_BROKER_PASSWORD,
-        # broker_token=PACT_BROKER_TOKEN,
+        broker_password=PACT_BROKER_PASSWORD
     )
 
     pact.start_service()
-
-    # Make sure the Pact mocked provider is stopped when we finish, otherwise
-    # port 1234 may become blocked
     atexit.register(pact.stop_service)
-
     yield pact
-
-    # This will stop the Pact mock server, and if publish is True, submit Pacts
-    # to the Pact Broker
     pact.stop_service()
-
-    # Given we have cleanly stopped the service, we do not want to re-submit the
-    # Pacts to the Pact Broker again atexit, since the Broker may no longer be
-    # available if it has been started using the --run-broker option, as it will
-    # have been torn down at that point
-    pact.publish_to_broker = False
+    pact.publish_to_broker = True
 
 
-def test_request_ads_with_valid_pub_app_and_placement(pact, consumer):
-    # Define the Matcher; the expected structure and content of the response
+def test_jaeger_responds_normally_without_sleep_code(pact, ads_consumer):
     test_pub_app = '59786bc2a43b3a08620026b1'
     test_placement = 'DEFAULT02021'
-    expected = expected_served_ads_response(placement_id=test_placement)
+    expected = expected_served_ads_response(placement_id=test_placement, header_bidding=True)
+    device_id = gen_device_id()
 
-    # Define the expected behaviour of the Provider. This determines how the
-    # Pact mock provider will behave. In this case, we expect a body which is
-    # "Like" the structure defined above. This means the mock provider will
-    # return the EXACT content where defined, e.g. UserA for name, and SOME
-    # appropriate content e.g. for ip_address.
+    req = jaeger_v5_ios(test_pub_app, test_placement, ifa=device_id, header_bidding=True)
+    headers = platform_headers(src_ip=au_ip, rtb_selector=non_test_mode_ext_kraken)
+
     (
-        pact.given("Test to request ads with a valid pub app")
+        pact.given("Test Jaeger responds normally without sleep code")
             .upon_receiving("get ads response from the Jaeger service")
-            .with_request("post", "/api/v5/ads", body=jaeger_v5_ios(test_pub_app, test_placement),
-                          headers=platform_headers(src_ip=au_ip, rtb_selector=non_test_mode_ext_kraken))
+            .with_request("post", "/api/v5/ads", body=req, headers=headers)
             .will_respond_with(200, body=Like(expected))
     )
 
     with pact:
-        # Perform the actual request
-        ads = consumer.request_ads(test_pub_app, test_placement)
-
-        # In this case the mock Provider will have returned a valid response
-        assert ads.placement_reference_id == test_placement
-
-        # Make sure that all interactions defined occurred
+        ads = ads_consumer.request_ads(req, headers)
+        # assert that there is no sleep code from ads response
+        assert ads.sleep_code is None
         pact.verify()
 
 
-def test_request_ads_with_non_existing_pub_app(pact, consumer):
-    test_pub_app = '123456'
-    test_placement = 'DEFAULT02021'
-    expected = expected_non_served_ads_response(sleep_code=901, info='publisher not found')
-
-    # Define the expected behaviour of the Provider. This determines how the
-    # Pact mock provider will behave. In this case, we expect a 404
-    (
-        pact.given("Test to get the config with a non-existing pub app")
-            .upon_receiving("get ads response from the Jaeger service")
-            .with_request("post", "/api/v5/ads", body=jaeger_v5_ios(test_pub_app, test_placement),
-                          headers=platform_headers(src_ip=au_ip, rtb_selector=non_test_mode_ext_kraken))
-            .will_respond_with(200, body=Like(expected))
-    )
-
-    with pact:
-        # Perform the actual request
-        ads = consumer.request_ads(test_pub_app, test_placement)
-
-        # In this case, the mock Provider will have returned a sleep code
-        assert ads.ad_markup['sleep'] == 901
-        assert ads.ad_markup['info'] == 'publisher not found'
-
-        # Make sure that all interactions defined occurred
-        pact.verify()
-
-
-def test_request_ads_with_non_existing_placement(pact, consumer):
+def test_jaeger_responds_normally_without_sleep_code_negative(pact, ads_consumer):
     test_pub_app = '59786bc2a43b3a08620026b1'
-    test_placement = '123465'
-    expected = expected_non_served_ads_response(sleep_code=902, info='placement not found')
+    test_placement = '123456'
+    expected = expected_served_ads_response(placement_id=test_placement, header_bidding=True)
+    device_id = gen_device_id()
 
-    # Define the expected behaviour of the Provider. This determines how the
-    # Pact mock provider will behave. In this case, we expect a 404
+    req = jaeger_v5_ios(test_pub_app, test_placement, ifa=device_id, header_bidding=True)
+    headers = platform_headers(src_ip=au_ip, rtb_selector=non_test_mode_ext_kraken)
+
     (
-        pact.given("Test to get the config with a non-existing placement")
+        pact.given("Test Jaeger responds normally without sleep code")
             .upon_receiving("get ads response from the Jaeger service")
-            .with_request("post", "/api/v5/ads", body=jaeger_v5_ios(test_pub_app, test_placement),
-                          headers=platform_headers(src_ip=au_ip, rtb_selector=non_test_mode_ext_kraken))
+            .with_request("post", "/api/v5/ads", body=req, headers=headers)
             .will_respond_with(200, body=Like(expected))
     )
 
     with pact:
-        # Perform the actual request
-        ads = consumer.request_ads(test_pub_app, test_placement)
-
-        # In this case, the mock Provider will have returned a sleep code
-        assert ads.ad_markup['sleep'] == 902
-        assert ads.ad_markup['info'] == 'placement not found'
-
-        # Make sure that all interactions defined occurred
+        ads = ads_consumer.request_ads(req, headers)
+        # assert that there is no sleep code from ads response
+        assert ads.sleep_code is None
         pact.verify()
+
+
+def test_jaeger_responds_normally_with_omsdk_data(pact, ads_consumer):
+    test_pub_app = '59786bc2a43b3a08620026b1'
+    test_placement = 'DEFAULT02021'
+    expected = expected_served_ads_response(placement_id=test_placement, header_bidding=True, omsdk=True)
+    device_id = gen_device_id()
+
+    req = jaeger_v5_ios(test_pub_app, test_placement, ifa=device_id, header_bidding=True)
+    headers = platform_headers(src_ip=au_ip, rtb_selector=non_test_mode_ext_kraken)
+
+    (
+        pact.given("Test Jaeger responds normally without sleep code")
+            .upon_receiving("get ads response from the Jaeger service")
+            .with_request("post", "/api/v5/ads", body=req, headers=headers)
+            .will_respond_with(200, body=Like(expected))
+    )
+
+    with pact:
+        ads = ads_consumer.request_ads(req, headers)
+        # assert that there is om data from the ads response
+        assert 'OM_SDK_DATA' in ads.ad_markup['templateSettings']['normal_replacements']
+        assert ads.ad_markup['viewability']['om']['is_enabled'] is True
+        pact.verify()
+
+
+def test_jaeger_responds_normally_with_omsdk_data_negative(pact, ads_consumer):
+    test_pub_app = '59786bc2a43b3a08620026b2'  # not a om enabled pub app
+    test_placement = 'DEFAULT02022'
+    expected = expected_served_ads_response(placement_id=test_placement, header_bidding=True, omsdk=True)
+    device_id = gen_device_id()
+
+    req = jaeger_v5_ios(test_pub_app, test_placement, ifa=device_id, header_bidding=True)
+    headers = platform_headers(src_ip=au_ip, rtb_selector=non_test_mode_ext_kraken)
+
+    (
+        pact.given("Test Jaeger responds normally without sleep code")
+            .upon_receiving("get ads response from the Jaeger service")
+            .with_request("post", "/api/v5/ads", body=req, headers=headers)
+            .will_respond_with(200, body=Like(expected))
+    )
+
+    with pact:
+        ads = ads_consumer.request_ads(req, headers)
+        # assert that there is om data from the ads response
+        assert 'OM_SDK_DATA' in ads.ad_markup['templateSettings']['normal_replacements']
+        assert ads.ad_markup['viewability']['om']['is_enabled'] is True
+        pact.verify()
+
+
+def test_jaeger_responds_normally_with_skan_data(pact, ads_consumer):
+    test_pub_app = '59786bc2a43b3a08620026b2'
+    test_placement = 'DEFAULT02022'
+    expected = expected_served_ads_response(placement_id=test_placement, skan=True)
+    device_id = test_mode_device_id
+    network_ids = [kraken_served_ad_network_id, 'test.ad.nw.001']
+    osv = '14.5'
+    sdkv = 'Vungle/6.10.0'
+
+    req = jaeger_v5_ios(test_pub_app, test_placement, ifa=device_id, os_version=osv, skadnetwork_ids=network_ids)
+    headers = platform_headers(src_ip=au_ip, rtb_selector=test_mode_kraken_rtb_id, sdk_version=sdkv)
+
+    (
+        pact.given("Test Jaeger responds normally without sleep code")
+            .upon_receiving("get ads response from the Jaeger service")
+            .with_request("post", "/api/v5/ads", body=req, headers=headers)
+            .will_respond_with(200, body=Like(expected))
+    )
+
+    with pact:
+        ads = ads_consumer.request_ads(req, headers)
+        # assert the skan data from the ads response
+        assert 'attribution' in ads.ad_markup
+        pact.verify()
+
+
+def test_jaeger_responds_normally_with_skan_data_negative(pact, ads_consumer):
+    test_pub_app = '59786bc2a43b3a08620026b2'
+    test_placement = 'DEFAULT02022'
+    expected = expected_served_ads_response(placement_id=test_placement, skan=True)
+    device_id = test_mode_device_id
+    network_ids = ['test.ad.nw.001']    # non-matched network id
+    osv = '14.5'
+    sdkv = 'Vungle/6.10.0'
+
+    req = jaeger_v5_ios(test_pub_app, test_placement, ifa=device_id, os_version=osv, skadnetwork_ids=network_ids)
+    headers = platform_headers(src_ip=au_ip, rtb_selector=test_mode_kraken_rtb_id, sdk_version=sdkv)
+
+    (
+        pact.given("Test Jaeger responds normally without sleep code")
+            .upon_receiving("get ads response from the Jaeger service")
+            .with_request("post", "/api/v5/ads", body=req, headers=headers)
+            .will_respond_with(200, body=Like(expected))
+    )
+
+    with pact:
+        ads = ads_consumer.request_ads(req, headers)
+        # assert the skan data from the ads response
+        assert 'attribution' in ads.ad_markup
+        pact.verify()
+
+
+# def test_request_ads_with_non_existing_pub_app(pact, ads_consumer):
+#     test_pub_app = '123456'
+#     test_placement = 'DEFAULT02021'
+#     expected = expected_non_served_ads_response(sleep_code=901, info='publisher not found')
+#     device_id = gen_device_id()
+#
+#     # Define the expected behaviour of the Provider. This determines how the
+#     # Pact mock provider will behave. In this case, we expect a 404
+#     (
+#         pact.given("Test to get the config with a non-existing pub app")
+#             .upon_receiving("get ads response from the Jaeger service 2")
+#             .with_request("post", "/api/v5/ads", body=jaeger_v5_ios(test_pub_app, test_placement, ifa=device_id),
+#                           headers=platform_headers(src_ip=au_ip, rtb_selector=non_test_mode_ext_kraken))
+#             .will_respond_with(200, body=Like(expected))
+#     )
+#
+#     with pact:
+#         # Perform the actual request
+#         ads = ads_consumer.request_ads(test_pub_app, test_placement, device_id=device_id)
+#
+#         # In this case, the mock Provider will have returned a sleep code
+#         assert ads.ad_markup['sleep'] == 901
+#         assert ads.ad_markup['info'] == 'publisher not found'
+#
+#         # Make sure that all interactions defined occurred
+#         pact.verify()
+#
+#
+# def test_request_ads_with_non_existing_placement(pact, ads_consumer):
+#     test_pub_app = '59786bc2a43b3a08620026b1'
+#     test_placement = '123465'
+#     expected = expected_non_served_ads_response(sleep_code=902, info='placement not found')
+#     device_id = gen_device_id()
+#
+#     # Define the expected behaviour of the Provider. This determines how the
+#     # Pact mock provider will behave. In this case, we expect a 404
+#     (
+#         pact.given("Test to get the config with a non-existing placement")
+#             .upon_receiving("get ads response from the Jaeger service 3")
+#             .with_request("post", "/api/v5/ads", body=jaeger_v5_ios(test_pub_app, test_placement, ifa=device_id),
+#                           headers=platform_headers(src_ip=au_ip, rtb_selector=non_test_mode_ext_kraken))
+#             .will_respond_with(200, body=Like(expected))
+#     )
+#
+#     with pact:
+#         # Perform the actual request
+#         ads = ads_consumer.request_ads(test_pub_app, test_placement, device_id=device_id)
+#
+#         # In this case, the mock Provider will have returned a sleep code
+#         assert ads.ad_markup['sleep'] == 902
+#         assert ads.ad_markup['info'] == 'placement not found'
+#
+#         # Make sure that all interactions defined occurred
+#         pact.verify()
