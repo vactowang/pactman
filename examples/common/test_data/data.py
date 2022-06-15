50e8634954b9a859fecb4ba64c33236d3700b2ba
vacto.wang <vacto.wang@vungle.com>
2022-06-15 16:17:21 +0800

    modify demo tests

--- examples/common/test_data/data.py
+++ examples/common/test_data/data.py
@@ -3,3 +3,11 @@ au_ip = '45.117.100.153'
 
 # rtb connections
 non_test_mode_ext_kraken = '5fd21d91c80cb9051249a6b1'
+test_mode_kraken_rtb_id = '5fd219d7c80cb9051249a6ab'
+hb_meister_rtb_id = '60adc79dfb70f80016e36884'
+legacy_meister_rtb_id = '59128f4fa8e4b2cbcb250c87'
+meister_rtb_ids = '59128f4fa8e4b2cbcb250c87,60adc79dfb70f80016e36884'
+
+test_mode_device_id = '4423DD36-2738-46DC-84D1-02A47F95320D'
+
+kraken_served_ad_network_id = 'APZHY3VA96.skadnetwork'
