089f00de3ede886f7da222f2f604017a543e0ce8
vacto.wang <vacto.wang@vungle.com>
2022-06-30 11:16:42 +0800

    modify

--- examples/consumer/run_complete_flow.sh
+++ examples/consumer/run_complete_flow.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 set -o pipefail
 
-PACTNAME=$1
-PROVIDERVERSION=$2
-SERVICE=$3
+CONSUMER_VERSION=$1
+PROVIDER_VERSION=$2
+PROVIDER_NAME=$3
 TEST=$4
 
 echo "Run pytest from SDK"
-pytest tests/consumer/test_ads_consumer.py::$TEST --publish-pact $PACTNAME
+pytest tests/consumer/test_ads_consumer.py::$TEST --publish-pact $CONSUMER_VERSION
 
 echo "Run test from SSP"
 cd ../provider/
-pytest tests/provider/test_ads_provider.py --publish-pact $PACTNAME
+pytest tests/provider/test_ads_provider.py --publish-pact $CONSUMER_VERSION
 
 echo "Validating against Pact Broker"
 pact-verifier \
-  --provider-base-url=http://apiqa.vungle.com \
-  --provider-app-version $PROVIDERVERSION \
-  --pact-url="http://127.0.0.1/pacts/provider/"$SERVICE"Service/consumer/SDK/latest" \
+  --provider-base-url=https://adex.ads-qa.vungle.com \
+  --provider-app-version $PROVIDER_VERSION \
+  --pact-url="http://127.0.0.1/pacts/provider/"$PROVIDER_NAME"Service/consumer/SDK/latest" \
   --pact-broker-username pactbroker \
   --pact-broker-password pactbroker \
   --publish-verification-results \
